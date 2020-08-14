e2f43c00718865bd610a5b1818581a20ffadce44
Janni9009 <33988244+Janni9009@users.noreply.github.com>
2020-08-14 23:54:06 +0200

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(MuteBlinkBeGoneMod.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(MuteBlinkBeGoneMod.BuildInfo.Company)]
+[assembly: AssemblyProduct(MuteBlinkBeGoneMod.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + MuteBlinkBeGoneMod.BuildInfo.Author)]
+[assembly: AssemblyTrademark(MuteBlinkBeGoneMod.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion(MuteBlinkBeGoneMod.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(MuteBlinkBeGoneMod.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(MuteBlinkBeGoneMod.MuteBlinkBeGone), MuteBlinkBeGoneMod.BuildInfo.Name, MuteBlinkBeGoneMod.BuildInfo.Version, MuteBlinkBeGoneMod.BuildInfo.Author, MuteBlinkBeGoneMod.BuildInfo.DownloadLink)]
+[assembly: MelonGame("VRChat", "VRChat")]
---
+++ Main.cs
@@ -0,0 +1,28 @@
+﻿using MelonLoader;
+using System;
+using System.Reflection;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using UnhollowerRuntimeLib;
+using System.Linq;
+using Harmony;
+using MelonLoader.Tomlyn;
+
+namespace MuteBlinkBeGoneMod
+{
+    public static class BuildInfo {
+        public const string Name = "MuteBlinkBeGone";
+        public const string Author = "Janni9009";
+        public const string Company = "VRChat Modding Group";
+        public const string Version = "1.0";
+        public const string DownloadLink = null;
+    }
+    public class MuteBlinkBeGone : MelonMod {
+        public override void VRChat_OnUiManagerInit() {
+            GameObject.Find("UserInterface/UnscaledUI/HudContent/Hud/VoiceDotParent/VoiceDotDisabled").GetComponent<FadeCycleEffect>().enabled = false;
+        }
+    }
+}
